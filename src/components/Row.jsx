e2747977e62b29068118e162de79c3665371ca4e
Maximo-4ever <maximilianocm06@gmail.com>
2022-02-07 21:37:12 -0600

    Agraga la key a img

--- src/components/Row.jsx
+++ src/components/Row.jsx
@@ -4,12 +4,11 @@ import "./Row.css";
 
 const base_url = "https://image.tmdb.org/t/p/original/";
 
-function Row({ title, fetchUrl }) {
+function Row({ title, fetchUrl, isLargeRow = false }) {
   const [movies, setMovies] = useState([]);
   useEffect(() => {
     const fetchData = async () => {
       const request = await axios.get(fetchUrl);
-      console.log(request.data.results);
       setMovies(request.data.results);
       return request;
     };
@@ -22,9 +21,12 @@ function Row({ title, fetchUrl }) {
       <div className="row__posters">
         {movies.map((movie) => (
           <img
-            src={base_url + movie.poster_path}
+            key={movie.id}
+            src={`${base_url}${
+              isLargeRow ? movie.poster_path : movie.backdrop_path
+            }`}
             alt={movie.name}
-            className="row__poster"
+            className={`row__poster ${isLargeRow && "row__posterLarge"}`}
           />
         ))}
       </div>
