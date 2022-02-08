7aa2d4082923fe906fff890a1e83880c1b5e27f8
Maximo-4ever <maximilianocm06@gmail.com>
2022-02-07 21:45:41 -0600

    Añade el banner y agrega mas generos de peliculas

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
-import './App.css';
-import Row from './components/Row';
-import requests from './requests';
+import "./App.css";
+import Banner from "./components/Banner";
+import Row from "./components/Row";
+import requests from "./requests";
 
 function App() {
   return (
     <div className="App">
-      <h1>Netflix Clone</h1>
-      <Row title="NETFLIX ORIGINALS" fetchUrl={requests.fetchNetflixOriginals} />
+      <Banner fetchUrl={requests.fetchNetflixOriginals} />
+      <Row
+        title="NETFLIX ORIGINALS"
+        fetchUrl={requests.fetchNetflixOriginals}
+        isLargeRow={true}
+      />
       <Row title="Trending now" fetchUrl={requests.fetchTrending} />
+      <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
+      <Row title="Action movies" fetchUrl={requests.fetchActionMovies} />
+      <Row title="Comedy movies" fetchUrl={requests.fetchComedyMovies} />
+      <Row title="Horror movies" fetchUrl={requests.fetchHorrorMovies} />
+      <Row title="Romance movies" fetchUrl={requests.fetchRomanceMovies} />
+      <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
     </div>
   );
 }
