c07a121b70a4e5c4e3a5735c1d7252b3e8a05532
Maximo-4ever <maximilianocm06@gmail.com>
2022-02-09 12:13:24 -0600

    Agrega el Navbar al layout de App

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
 import "./App.css";
+import Navbar from "./components/Navbar";
 import Banner from "./components/Banner";
 import Row from "./components/Row";
 import requests from "./requests";
 
 function App() {
   return (
-    <div className="App">
+    <>
+      <Navbar />
       <Banner fetchUrl={requests.fetchNetflixOriginals} />
-      <Row
-        title="NETFLIX ORIGINALS"
-        fetchUrl={requests.fetchNetflixOriginals}
-        isLargeRow={true}
-      />
-      <Row title="Trending now" fetchUrl={requests.fetchTrending} />
-      <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
-      <Row title="Action movies" fetchUrl={requests.fetchActionMovies} />
-      <Row title="Comedy movies" fetchUrl={requests.fetchComedyMovies} />
-      <Row title="Horror movies" fetchUrl={requests.fetchHorrorMovies} />
-      <Row title="Romance movies" fetchUrl={requests.fetchRomanceMovies} />
-      <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
-    </div>
+      <main className="container">
+        <Row
+          title="NETFLIX ORIGINALS"
+          fetchUrl={requests.fetchNetflixOriginals}
+          isLargeRow={true}
+        />
+        <Row title="Trending now" fetchUrl={requests.fetchTrending} />
+        <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
+        <Row title="Action movies" fetchUrl={requests.fetchActionMovies} />
+        <Row title="Comedy movies" fetchUrl={requests.fetchComedyMovies} />
+        <Row title="Horror movies" fetchUrl={requests.fetchHorrorMovies} />
+        <Row title="Romance movies" fetchUrl={requests.fetchRomanceMovies} />
+        <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
+      </main>
+    </>
   );
 }
 
