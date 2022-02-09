3d8d584c4a266a4a31dd9a58d4178d8cf217dd25
Maximo-4ever <maximilianocm06@gmail.com>
2022-02-09 12:12:06 -0600

    Añade y estiliza el navbar en los componentes

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+import "./Navbar.css";
+
+function Navbar() {
+  return (
+    <nav className="nav">
+      <img
+        className="nav__logo"
+        src="https://logos-marcas.com/wp-content/uploads/2020/04/Netflix-Logo.png"
+        alt="Netflix logo"
+      />
+    </nav>
+  );
+}
+
+export default Navbar;
